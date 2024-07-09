9cd74965cd17c204be7b9dc06a56d9c221e40947
aichemeco <denghao@aichemeco.com>
2024-07-09 18:03:07 +0800

    Update model.py

--- model.py
+++ model.py
@@ -3,11 +3,11 @@ from XGBoost import XGBoost
 from RandomForest import RandomForest
 
 
-def train_machine_learning_model(model,df,sub1_column, sub2_column, product_column)
-    if model == "SVM":
+def train_machine_learning_model(model_type,df,sub1_column, sub2_column, product_column)
+    if model_type == "SVM":
         SVM(df, sub1_column, sub2_column, product_column) #df is yourdataset, sub1_column is the tiele of ammonia column, sub2_column is the title of acid column and product_column is the title of product column.
-    elif model == "XGBoost":
+    elif model_type == "XGBoost":
         XGBoost(df, sub1_column, sub2_column, product_column)
-    elif model == "RandomForest":
+    elif model_type == "RandomForest":
         RandomForest(df, sub1_column, sub2_column, product_column)
     return
